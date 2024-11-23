37b03bc0f81c7fd7b22a4314d57e7c12474cfc3d
Nourin Jahan Sonda <nourin.jahan@bracits.com>
2024-11-24 02:02:38 +0600

    Assignment 1

---
+++ src/StudentResult.java
@@ -0,0 +1,19 @@
+abstract class StudentResult {
+    private String firstYearResult;
+    private String secondYearResult;
+
+    public void setFirstYearResult(String firstYearResult) {
+        this.firstYearResult = firstYearResult;
+    }
+    public String getFirstYearResult () {
+        return firstYearResult;
+    }
+
+    public void setSecondYearResult(String secondYearResult) {
+        this.secondYearResult = secondYearResult;
+    }
+
+    public String getSecondYearResult() {
+        return secondYearResult;
+    }
+}
